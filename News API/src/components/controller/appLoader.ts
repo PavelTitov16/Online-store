62e5a5efdda76e2ee1e220e2cd9bfc2fd8c57187
PavelTitov16 <vitaliya7-5@mail.ru>
2022-07-04 16:28:18 +0300

    feat: add footer with github and couse link

--- News API/src/components/controller/appLoader.ts
+++ News API/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: 'f4cb2e3c3b5c4e2bb1db19fce6f1f2e4', 
+            apiKey: '89616b3c014648d4ac3ecb5fea05b48b', 
         });
     }
 }
