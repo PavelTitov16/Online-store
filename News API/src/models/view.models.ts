6cac499b0eac187f42e55645c5bc72a6a17db52d
PavelTitov16 <pashoktitov16@gmail.com>
2022-07-05 21:39:07 +0300

    refactor: delete useless comments in view model file

--- News API/src/models/view.models.ts
+++ News API/src/models/view.models.ts
@@ -1,5 +1,3 @@
-/*import News from '../components/view/news/news';
-import Sources from '../components/view/sources/sources';*/
 import { IArticle, IResponseArticles } from './article.models';
 import { IResponseSources, ISourceData } from './source.models';
 import { DrawModel } from './draw.model';
