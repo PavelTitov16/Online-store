9f2ed4855a2e0dd67eb39f383d26ab213e22c701
PavelTitov16 <pashoktitov16@gmail.com>
2022-07-05 04:32:53 +0300

    refactor: change api key source

--- News API/src/components/controller/appLoader.ts
+++ News API/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '89616b3c014648d4ac3ecb5fea05b48b', 
         });
     }
