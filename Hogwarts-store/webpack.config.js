f1340436984d27db5f5e18e76aeefc456017e860
PavelTitov16 <pashoktitov16@gmail.com>
2022-07-07 01:43:01 +0300

    init: start online store task and init project packages

---
+++ Hogwarts-store/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const EslintPlugin = require('eslint-webpack-plugin'); 
+const isProduction = process.env.NODE_ENV == 'production';
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
+    entry: './src/index',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        
+
+        new MiniCssExtractPlugin(),
+        new EslintPlugin({ extensions: 'ts' })
+        
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/i, 
+                use: 'ts-loader',
+                exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler, 'css-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
