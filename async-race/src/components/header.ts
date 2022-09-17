97159d5d73842c3e299fcbaaba6ef56d9fca1970
PavelTitov16 <pashoktitov16@gmail.com>
2022-09-17 19:52:45 +0300

    refactor: add adaptive for header

--- async-race/src/components/header.ts
+++ async-race/src/components/header.ts
@@ -10,17 +10,18 @@ export class Header implements HeaderModel {
                 <h1 class="header-container__title" id="app-title">
                     Need for Promise
                 </h1>
-                <div class="audio-payer">
-                  <audio class="audio" id="audio"></audio>
-                  <div class="controls">
-                    <div id="prev" class="audio-payer__btn prev"></div>
-                    <div id="play" class="audio-payer__btn play"></div>
-                    <div id="next" class="audio-payer__btn next"></div>
+                <div class="header-container__items">
+                  <div class="audio-payer">
+                    <audio class="audio" id="audio"></audio>
+                    <div class="controls">
+                      <div id="prev" class="audio-payer__btn prev"></div>
+                      <div id="play" class="audio-payer__btn play"></div>
+                      <div id="next" class="audio-payer__btn next"></div>
+                    </div>
+                    <progress class="progress-bar" id="progress-bar" value="0" max="1"></progress>
+                    <div id="mute" class="audio-payer__btn mute"></div>
                   </div>
-                  <progress class="progress-bar" id="progress-bar" value="0" max="1"></progress>
-                  <div id="mute" class="audio-payer__btn mute"></div>
-                </div>
-                <nav class="header-nav">
+                  <nav class="header-nav">
                     <div class="header-nav__item">
                         <button id="garage-btn" class="header__btn button">
                             Garage
@@ -31,7 +32,8 @@ export class Header implements HeaderModel {
                             Winners
                         </button>
                     </div>
-                </nav>
+                  </nav>
+                </div>
             </div>
         </header>`;
     return this.template;
